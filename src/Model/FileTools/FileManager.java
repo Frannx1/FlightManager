f3db7f16085580e43e27258ba8e0b4472dca002f
alejoaquili <aaquili@itba.edu.ar>
2017-10-31 19:35:31 -0300

    parser

--- src/Model/FileTools/FileManager.java
+++ src/Model/FileTools/FileManager.java
@@ -1,23 +1,13 @@
-/*
+
 package Model.FileTools;
-*/
-/*
+
 import Model.trash.AirportManager;
-//import Model.trash.Airport;
-import Model.trash.Flight;*//*
-
-
-import Model.Graph.AirportGraph.Structures.Airport;
 import Model.Graph.AirportGraph.Structures.Day;
-import Model.Graph.AirportGraph.Structures.Flight;
 import Model.Graph.GraphStructures.Node;
-import Model.Graph.AirportGraph.AirportManager;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
---
+++ src/Model/FlightPriority.java
@@ -0,0 +1,5 @@
+package Model;
+
+public enum  FlightPriority {
+    TIME, PRICE, TOTAL_TIME;
+}
